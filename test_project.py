13816241b2a818aee78b78f79acb2f9cf5d15e0e
juliana1996 <julizuoso@gmail.com>
2016-11-06 15:51:41 -0500

    prueba

--- test_project.py
+++ test_project.py
@@ -1,4 +1,10 @@
 from python import crear, listado, eliminar_files, listar_recientes
 
-def test_crear():
-    assert crear()== 201
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 5
+
+#def test_crear():
+#    assert crear()== 201
