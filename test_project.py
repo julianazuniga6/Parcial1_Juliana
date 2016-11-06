3b3b7157167e8994c98c9204b7e258107cdf63d0
juliana1996 <julizuoso@gmail.com>
2016-11-06 16:18:24 -0500

    prueba de funcionamiento de request

--- test_project.py
+++ test_project.py
@@ -1,10 +1,15 @@
 from python import crear, listado, eliminar_files, listar_recientes
+import flask
 
-def func(x):
-    return x + 1
+app = Flask(__name__)
+app.test_request_context('/?filename=Pete&content=estavainafunciona')
 
-def test_answer():
-    assert func(3) == 5
 
-#def test_crear():
-#    assert crear()== 201
+#def func(x):
+#   return x + 1
+
+#def test_answer():
+#    assert func(3) == 5
+
+def test_crear():
+    assert crear()== 201
