9819138354f8873fa05874edfd42d1f1a33b6623
julizuoso <julizuoso@gmail.com>
2016-11-06 15:57:45 -0500

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,10 +1,10 @@
 from python import crear, listado, eliminar_files, listar_recientes
 
-def func(x):
-    return x + 1
+#def func(x):
+#    return x + 1
+#
+#def test_answer():
+#    assert func(4) == 5
 
-def test_answer():
-    assert func(4) == 5
-
-#def test_crear():
-#    assert crear()== 201
+def test_crear():
+    assert crear()== 201
