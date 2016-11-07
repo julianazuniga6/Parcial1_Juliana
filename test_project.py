4bd564fed297976e56d2b4b2d73ed8462de27b13
juliana1996 <julizuoso@gmail.com>
2016-11-06 22:19:06 -0500

    prueba crear archivo

--- test_project.py
+++ test_project.py
@@ -7,6 +7,8 @@ def client(request):
     client = python.app.test_client()
     return client
 
+
+
 def get_files(client):
 	return client.get('/files',follow_redirects=True)
 
@@ -14,16 +16,21 @@ def test_get_files(client):
 	result = get_files(client)
 	assert result.data != None, "Error getting file list"
 
+
+
 def create_file(client):
 	return client.post('/files',data=json.dumps(dict(filename='test_file',content='It is working')),content_type='application/json')
 
 def test_create_file(client):
 	result= create_file(client)
-	assert result.status == "201 CREATED", "Error creating file"
+	assert result == "HTTP 201 CREATED- se creo el archivo", "Error creating file"
+
+
+
 
 def delete_files(client):
 	return client.delete('/files',follow_redirects=True)
 
 def test_delete_files(client):
 	result= delete_files(client)
-	assert result.status == "200 OK","Error deleting all files"
+	assert result == "HTTP 200 OK- SE ELIMINARON LOS ARCHIVOS DE filesystem_user","Error deleting all files"
